d199e5a3787c5f3b1ba00e7a7f55166f4b2ae801
Muhammadamin <augustin.coder@gmail.com>
2026-04-18 01:14:19 +0500

    Maxfiy ma'lumotlarsiz toza commit

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://wsvzggnhotzhmvugeyil.supabase.co'
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_secret_nZQcnSza0XFCGy_4k1o8rw_zmJ_ISgM'
+// const SUPABASE_URL = process.env.SUPABASE_URL
+// const SUPABASE_KEY = process.env.SUPABASE_KEY
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+// export const supabase = createClient(SUPABASE_URL!, SUPABASE_KEY!)
